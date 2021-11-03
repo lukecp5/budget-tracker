19fbe238f00d7035b9463740cef09d89de739d32
Luke Poirrier <lukepoirrier5@gmail.com>
2021-11-03 14:46:33 -0400

    Refactor db.js to functionally work better

--- public/db.js
+++ public/db.js
@@ -3,14 +3,14 @@ const iDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB ||
 let db;
 const request = iDB.open("budgetDB", 1);
 
+request.onupgradeneeded = ({ target }) => {
+      let db = target.result;
+      db.createObjectStore("pending", { autoIncrement: true });
+    };
+
 function saveRecord(record) {
       const transaction = db.transaction(["pending"], "readwrite");
       const store = transaction.objectStore("pending");
     
       store.add(record);
     }
-
-request.onupgradeneeded = ({ target }) => {
-      let db = target.result;
-      db.createObjectStore("pending", { autoIncrement: true });
-    };
