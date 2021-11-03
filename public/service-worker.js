ce4296110c2b1ad585ef2d1651452b1cfeceed58
Luke Poirrier <lukepoirrier5@gmail.com>
2021-11-03 15:59:52 -0400

    Add a list of URLs for the service worker to cache

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,13 @@
 var CACHE = "budget-site-cache";
 const DATA_CACHE = "budget-site-data-cache";
 
-var URLS_TO_CACHE = [];
+var URLS_TO_CACHE = [
+      "/",
+      "/index.html",
+      "/db.js",
+      "/styles.css",
+      "/manifest.json",
+      "/index.js",
+      "/icons/icon-192x192.png",
+      "/icons/icon-512x512.png"
+];
