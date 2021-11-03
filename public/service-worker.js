d6aa54f81c921c36b694fe1d5b28bbffb738a6ce
Luke Poirrier <lukepoirrier5@gmail.com>
2021-11-03 16:07:37 -0400

    Refactor URLS_TO_CACHE

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,14 +2,14 @@ var CACHE = "budget-site-cache";
 const DATA_CACHE = "budget-site-data-cache";
 
 var URLS_TO_CACHE = [
-      "/",
-      "/index.html",
-      "/db.js",
-      "/styles.css",
-      "/manifest.json",
-      "/index.js",
-      "/icons/icon-192x192.png",
-      "/icons/icon-512x512.png"
+	"/",
+	"/index.html",
+	"/db.js",
+	"/styles.css",
+	"/manifest.json",
+	"/index.js",
+	"/icons/icon-192x192.png",
+	"/icons/icon-512x512.png",
 ];
 
 self.addEventListener("install", function(event) {
