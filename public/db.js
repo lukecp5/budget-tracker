d15bbf3947264e11dd0564c3996defe42a3c188f
Luke Poirrier <lukepoirrier5@gmail.com>
2021-11-03 14:50:22 -0400

    Format code styling and add code to handle checking to see if the user is online

--- public/db.js
+++ public/db.js
@@ -3,14 +3,23 @@ const iDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB ||
 let db;
 const request = iDB.open("budgetDB", 1);
 
-request.onupgradeneeded = ({ target }) => {
-      let db = target.result;
-      db.createObjectStore("pending", { autoIncrement: true });
-    };
+request.onupgradeneeded = ({target}) => {
+	let db = target.result;
+	db.createObjectStore("pending", {autoIncrement: true});
+};
+
+request.onsuccess = ({target}) => {
+	db = target.result;
+
+	// check if app is online before reading from db
+	if (navigator.onLine) {
+		checkDatabase();
+	}
+};
 
 function saveRecord(record) {
-      const transaction = db.transaction(["pending"], "readwrite");
-      const pendingStore = transaction.objectStore("pending");
-    
-      pendingStore.add(record);
-    }
+	const transaction = db.transaction(["pending"], "readwrite");
+	const pendingStore = transaction.objectStore("pending");
+
+	pendingStore.add(record);
+}
