54dcd86631d065336beef2064990474cade964c6
Luke Poirrier <lukepoirrier5@gmail.com>
2021-11-04 15:41:36 -0400

    Update package.json for PWA and fix service workers sending requests to the API endpoints

--- public/db.js
+++ public/db.js
@@ -17,10 +17,8 @@ request.onsuccess = ({target}) => {
 
 // + Handle the error in the case of the retrieval of the records from the indexed DB
 request.onerror = (event) => {
-console.log(`\nREQUEST ERROR: \n${event.target}\n`);
-}
-
-
+	console.log(`\nREQUEST ERROR: \n${event.target}\n`);
+};
 
 function saveRecord(record) {
 	const transaction = db.transaction(["pending"], "readwrite");
@@ -30,35 +28,36 @@ function saveRecord(record) {
 }
 
 function investigateDatabase() {
-      // + Initialize the transaction 
-      const transaction = db.transaction(["pending"], "readwrite");
-      const pendingStore = transaction.objectStore("pending");
+	// + Initialize the transaction
+	const transaction = db.transaction(["pending"], "readwrite");
+	const pendingStore = transaction.objectStore("pending");
 
-      // + Create the cursor to iterate over the records
-      const allPending = pendingStore.getAll();
+	// + Create the cursor to iterate over the records
+	const allPending = pendingStore.getAll();
 
-      // + Handle the successful retrieval of the records from the indexed DB
-      allPending.onsuccess = () => {
-            // + If there are records in the indexed DB, then send them to the server for saving to the database
-            if(allPending.length > 0){
-                  fetch("/api/transaction/bulk", {
-                        method: "POST",
-                        body: JSON.stringify(allPending.result),
-                        headers: {
-                              Accept: "application/json",
-                              "Content-Type": "application/json"
-                        }
-                  })
-                  .then(response => response.json())
-                  .then(() => {
-                        // + If the records were sent to the server successfully, then clear the records from the indexed DB
-                        const transaction = db.transaction(["pending"], "readwrite");
-                        const pendingStore = transaction.objectStore("pending");
-                        pendingStore.clear();
-                  });
-            }
-      }
+	// + Handle the successful retrieval of the records from the indexed DB
+	allPending.onsuccess = () => {
+		// + If there are records in the indexed DB, then send them to the server for saving to the database
+		if (allPending.result.length > 0) {
+			fetch("/api/transaction/bulk", {
+				method: "POST",
+				body: JSON.stringify(allPending.result),
+				headers: {
+					Accept: "application/json",
+					"Content-Type": "application/json",
+				},
+			})
+				.then((response) => {
+					return response.json();
+				})
+				.then(() => {
+					// + If the records were sent to the server successfully, then clear the records from the indexed DB
+					const transaction = db.transaction(["pending"], "readwrite");
+					const pendingStore = transaction.objectStore("pending");
+					pendingStore.clear();
+				});
+		}
+	};
 }
-
 
 window.addEventListener("online", investigateDatabase);
